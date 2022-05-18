b1734f98ac3d92aacc99af09fc850334c12445fd
MatveyIvanov <cool.matvey250301@gmail.com>
2022-05-18 08:53:30 +0300

    Добавил обмен сообщениями в локалке через UDP. Отправка на удалённый IP пока не работает

---
+++ Client.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace DNS_Server
+{
+    class Client
+    {
+        static int localPort; // порт приема сообщений
+        static int remotePort; // порт для отправки сообщений
+        static string remoteIP;
+        static Socket listeningSocket;
+
+        public Client(int loPort, int rePort, string reIP)
+        {
+            localPort = loPort;
+            remotePort = rePort;
+            remoteIP = reIP;
+        }
+        public void start()
+        {
+            send();
+        }
+        private void send()
+        {
+            Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+            Console.WriteLine();
+            try
+            {
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                Task listeningTask = new Task(listen);
+                listeningTask.Start();
+
+                // отправка сообщений на разные порты
+                while (true)
+                {
+                    EndPoint remotePoint = new IPEndPoint(IPAddress.Parse(remoteIP), remotePort);
+                    listeningSocket.SendTo(UDP.Build(Console.ReadLine()), remotePoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                close();
+            }
+        }
+
+        // поток для приема подключений
+        private void listen()
+        {
+            try
+            {
+                //Прослушиваем по адресу
+                IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), localPort);
+                listeningSocket.Bind(localIP);
+
+                while (true)
+                {
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+
+                    //адрес, с которого пришли данные
+                    EndPoint remoteIp = new IPEndPoint(IPAddress.Parse(remoteIP), 53);
+
+                    do
+                    {
+                        bytes = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (listeningSocket.Available > 0);
+                    // получаем данные о подключении
+                    IPEndPoint remoteFullIp = remoteIp as IPEndPoint;
+
+                    // выводим сообщение
+                    Console.WriteLine("{0}:{1} - {2}", remoteFullIp.Address.ToString(),
+                                                    remoteFullIp.Port, builder.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                close();
+            }
+        }
+
+        // закрытие сокета
+        private static void close()
+        {
+            if (listeningSocket != null)
+            {
+                listeningSocket.Shutdown(SocketShutdown.Both);
+                listeningSocket.Close();
+                listeningSocket = null;
+            }
+        }
+    }
+}
---
+++ UDP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNS_Server
+{
+    enum DataType
+    {
+        Unicode,
+        ASCII
+    }
+    class UDP
+    {
+
+        public static byte[] Build(string message, DataType dataType = DataType.Unicode)
+        {
+            switch (dataType)
+            {
+                case DataType.Unicode:
+                {
+                    return Encoding.Unicode.GetBytes(message); 
+                }
+                case DataType.ASCII:
+                {
+                    return Encoding.ASCII.GetBytes(message);
+                }
+            }
+            return new byte[64];
+        }
+    }
+}
