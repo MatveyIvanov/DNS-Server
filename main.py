d3ea30e3da218daebfb0a4e0a3aef1f160411c4e
MatveyIvanov <cool.matvey250301@gmail.com>
2022-05-20 22:42:40 +0300

    Добавил возможность получения нескольких IPЭ

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import sys
+import re
 from dns_client.Client import Client
 from dns_client.exceptions import ValidationError
+
+
+MAX_QUERIES_TO_GET_NEW_IP = 15
+MANY_FLAGS = ('-a', '--all') # If set then check for more than 1 IP
 
 
 if __name__ == "__main__":
     # Get the host name from the command line.
     domain_name = ''
+    many = False
 
     try:
         domain_name = sys.argv[1]
@@ -16,24 +22,67 @@ if __name__ == "__main__":
 
         sys.exit(0)
 
-    DNS_Client = Client()
     try:
-        # Send packet
-        DNS_Client.send(domain_name=domain_name)
-        # Recieve packet
-        result = DNS_Client.recieve()
-    except ValidationError as e:
-        print(str(e))
+        many = sys.argv[2]
+        if many not in MANY_FLAGS:
+            print("Invalid flag. If you want to get more than 1 IP, then user -a or --all flag.")
 
-        sys.exit(0)
+            sys.exit(0)
+        many = True
+    except IndexError:
+        pass
+
+    result = {
+        'domain_name': None,
+        'ip_addresses': set(),
+        'errors': None
+    }
+
+    DNS_Client = Client()
+    repeat_count = 0
+    while True:
+        try:
+            # Send packet
+            DNS_Client.send(domain_name=domain_name)
+            # Recieve packet
+            data = DNS_Client.recieve()
+            if data['errors']:
+                result = data
+                break
+            
+            if data['ip_address'] in result['ip_addresses']:
+                repeat_count += 1
+                if repeat_count >= MAX_QUERIES_TO_GET_NEW_IP:
+                    break
+                continue
+            repeat_count = 0
+
+            if not result['domain_name']:
+                result['domain_name'] = data['domain_name']
+            result['ip_addresses'].add(data['ip_address'])
+
+            if not many:
+                break
+        except ValidationError as e:
+            print(str(e))
+
+            sys.exit(0)
 
     # Check for errors
-    if result['errors'] is not None:
+    if result['errors']:
         print(f"\n\
         There was an error while handling a dns query.\n\n\
         Error message: {result['errors']}")
     else:
         print(f"\n\
         Success!\n\n\
-        Domain name: {result['domain_name']}\n\
-        IP address: {result['ip_address']}")
+        Domain name: {result['domain_name']}")
+        if len(result['ip_addresses']) == 1:
+            print("\n\
+        IP address:   --------------\n", end='')
+        else:
+            print("\n\
+        IP addresses:   --------------\n", end='')
+        for ip in result['ip_addresses']:
+            print(f"\
+                        {ip}")
